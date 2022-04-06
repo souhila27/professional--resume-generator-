0eda45f24fe48ff1fc88645477c89b81d54f1c61
souhila boukari <suhamimisou14@gmail.com>
2022-04-06 19:33:28 -0400

    adding first  input which is the description - instalation -usage -license- contribution test user name email of the project

--- index.js
+++ index.js
@@ -6,20 +6,117 @@ const generateMarkdown = require('./utils/generateMarkdown');
 const fs = require('fs');
 const inquirer = require('inquirer');
 // TODO: Create an array of questions for user input
-const questions = [{
-    type: 'input',
-    name: 'title',
-    message: 'Enter the project title: (REQUIRED)',
-    validate: titleInput => {
-        if (titleInput) {
-            return true;
-        } else {
-            console.log('Please enter a project title!');
-            return false;
+const questions = [ 
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Enter the project title: (REQUIRED)',
+        validate: titleInput => {
+            if (titleInput) {
+                return true;
+            } else {
+                console.log('Please enter a project title!');
+                return false;
+            }
         }
-    }
-},
-
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Enter the project description: (REQUIRED)',
+        validate: descriptionInput => {
+            if (descriptionInput) {
+                return true;
+            } else {
+                console.log('Please enter a project description!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Enter the installation instructions: (REQUIRED)',
+        validate: installationInput => {
+            if (installationInput) {
+                return true;
+            } else {
+                console.log('Please enter installation instructions!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Enter usage information: (REQUIRED)',
+        validate: usageInput => {
+            if (usageInput) {
+                return true;
+            } else {
+                console.log('Please enter usage information!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'Enter contribution guidelines: (REQUIRED)',
+        validate: contributionInput => {
+            if (contributionInput) {
+                return true;
+            } else {
+                console.log('Please enter contribution information!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'Enter test instructions: (REQUIRED)',
+        validate: testInput => {
+            if (testInput) {
+                return true;
+            } else {
+                console.log('Please enter test instructions!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Please select a license:',
+        choices: ['Apache License 2.0', 'ISC License', 'MIT License']
+    },
+    {
+        type: 'input',
+        name: 'userName',
+        message: 'Enter your GitHub username: (REQUIRED)',
+        validate: userNameInput => {
+            if (userNameInput) {
+                return true;
+            } else {
+                console.log('Please enter a GitHub username!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Enter your email address: (REQUIRED)',
+        validate: emailInput => {
+            if (emailInput) {
+                return true;
+            } else {
+                console.log('Please enter an email address!');
+                return false;
+            }
+        }
+    },
 ];
 
 // TODO: Create a function to initialize app
